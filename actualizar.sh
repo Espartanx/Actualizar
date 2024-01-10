9bdc2df5041a1444a9fe206c11731419fb05e62e
DRAGON_FLY <119144727+Espartanx@users.noreply.github.com>
2024-01-09 22:55:14 -0600

    Add files via upload

---
+++ actualizar.sh
@@ -0,0 +1,128 @@
+#!bin/bash/
+
+#colores 
+colorn='\033[30m'
+colorr='\033[31m'
+colorv='\033[32m'
+coloram='\033[33m'
+colora='\033[34m'
+colorm='\033[35m'
+colort='\033[36m'
+colorb='\033[37m'
+fondon='\033[40m'
+fondor='\033[41m'
+fondov='\033[42m'
+fondoam='\033[43m'
+fondoa='\033[44m'
+fondom='\033[45m'
+fondot='\033[46m'
+fondog='\033[47m'
+colorres='\033[0m'
+#banner 
+
+echo -e ${colorr} "  _______  _______  ______  _______  ______  _______  _______  _______ ";
+echo -e ${colorr} " |    ___||     __||   __ \|   _   ||   __ \|_     _||   _   ||    |  | ";
+echo -e ${colora} " |    ___||__     ||    __/|       ||      <  |   |  |       ||       | ";
+echo -e ${colorv} " |_______||_______||___|   |___|___||___|__|  |___|  |___|___||__|____| ";
+sleep 1
+echo -e ${colorr} " se actualizaran y deacaegaran "
+echo " librerias de todo tipo disponibles"
+sleep 1
+echo -e ${colorres}
+echo "psdt"
+echo "faltan mas"
+sleep 1
+echo -e ${colora}
+echo "   _____                          _  _            _ ";
+echo "  / ____|                        (_)| |          | | ";
+echo " | (___   _   _  ___   ___  _ __  _ | |__    ___ | |_   ___ ";
+echo "  \___ \ | | | |/ __| / __|| '__|| || '_ \  / _ \| __| / _ \ ";
+echo "  ____) || |_| |\__ \| (__ | |   | || |_) ||  __/| |_ |  __/ ";
+echo " |_____/  \__,_||___/ \___||_|   |_||_.__/  \___| \__| \___| ";
+echo -e ${colorres}
+
+#url
+termux-open-url https://www.youtube.com/@dragonflybyte
+sleep 3
+echo -e ${fondoa}
+echo -e ${colorr}
+echo "  ____,  ____,   ____, ____,  __,    ____, ____, ";
+echo " (-/ _, (-|__)  (-/_| (-/    (-|    (-/_| (-(__ ";
+echo "  _\__|  _|  \, _/  |, _\__,  _|_,  _/  |, ____) ";
+echo " (      (      (      (      (     (      ( ";
+
+sleep 1
+
+echo -e ${colorres}
+
+
+echo "empezando instalacion"
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python2-dev -y
+pkg install python3 -y
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install lolcat
+pip install bs4
+pip2 install bs4
+pip2 install requests bs4
+pip install futures
+pip2 install futures
+pip install rich
+pkg install java -y
+pkg install fish -y
+pkg install ruby -y
+pkg install help -y
+pkg install git -y
+pkg install host -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install w3m -y
+pkg install havij -y
+pkg install hydra -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install google -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wireshark -y
+pkg install wgetrc -y
+pkg install wcalc -y
+pkg install bmon -y
+pkg install vpn -y
+pkg install unrar -y
+pkg install toilet -y
+pkg install proot -y
+pkg install net-tools -y
+pkg install golang -y
+pkg install chroot -y
+termux-chroot -y
+pkg install macchanger -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install wireshark -y
+termux-setup-storage -y
+pkg install macchanger -y
+apt update && apt upgrade -y
+pip install requirements
+pkg install espeak
+pip install espeak
+pip install mpv
+pip install --upgrade pip
+pkg install robot
+pip install --robota
